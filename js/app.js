7ec2c9471cb16a113ece0a4debb8e8e5a4e559dd
Tim Marks <timmarks90@gmail.com>
2018-04-27 07:15:28 -0700

    hid students, creating pagination

--- js/app.js
+++ js/app.js
@@ -1,12 +1,50 @@
-// Hide all but 10 students in list on page load
-const studentList = document.getElementsByClassName('student-item');
+const studentList = document.querySelectorAll('.student-item');
+const pageLimit = 10;
 
-function showPage(number) {
-  $(studentList).hide();
-  for(let i = 0; i < studentList.length; i++) {
-    if(studentList[i] <= number){
-      $(studentList[i]).show();
+// Hide all but 10 students in list on page load. Each page will only show 10 students.
+const showPage = (number, list) => {
+  for (let i = 0; i < list.length; i++) {
+    if (i >= (number * pageLimit) - 10 && i <= (number * pageLimit) - 1 ) {
+      list[i].style.display = 'block';
+    } else {
+      list[i].style.display = 'none';
     }
   }
 }
-showPage(10);
+
+showPage(1, studentList);
+
+// Calculate number of pages to show based off the total number of students in list
+const totalPages = Math.floor(studentList.length/pageLimit);
+
+// Add pagination to bottom of the page based off number of pages to show
+const pagination = pageLinks => {
+  const paginationHTML = document.querySelector('.pagination').innerHTML;
+  paginationHTML += "<ul>";
+  paginationHTML += "<li><a class="active" href="#">1</a></li>";
+  paginationHTML += "<li><a href="#">2</a></li>";
+  paginationHTML += "<li><a href="#">3</a></li>";
+  paginationHTML += "<li><a href="#">4</a></li>";
+  paginationHTML += "</ul>";
+}
+
+pagination(totalPages);
+
+// <ul>
+//           <li>
+//             <a class="active" href="#">1</a>
+//           </li>
+//            <li>
+//             <a href="#">2</a>
+//           </li>
+//            <li>
+//             <a href="#">3</a>
+//           </li>
+//            <li>
+//             <a href="#">4</a>
+//           </li>
+//            <li>
+//             <a href="#">5</a>
+//           </li>
+//         </ul>
+//       </div>
