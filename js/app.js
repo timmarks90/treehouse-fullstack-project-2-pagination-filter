1c6680e355c078753e3ad994a69357c2c70c535e
Tim Marks <timmarks90@gmail.com>
2018-04-29 10:49:22 -0700

    Added pagination
    Added dynamic pagination for project 2, showing active class for each page.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 const studentList = document.querySelectorAll('.student-item');
 const pageLimit = 10;
+const pageDiv = document.querySelector('.page');
+// Calculate number of pages to show based off the total number of students in list
+const totalPages = Math.floor(studentList.length / pageLimit);
 
 // Hide all but 10 students in list on page load. Each page will only show 10 students.
 const showPage = (number, list) => {
@@ -14,37 +17,37 @@ const showPage = (number, list) => {
 
 showPage(1, studentList);
 
-// Calculate number of pages to show based off the total number of students in list
-const totalPages = Math.floor(studentList.length/pageLimit);
-
 // Add pagination to bottom of the page based off number of pages to show
-const pagination = pageLinks => {
-  const paginationHTML = document.querySelector('.pagination').innerHTML;
-  paginationHTML += "<ul>";
-  paginationHTML += "<li><a class="active" href="#">1</a></li>";
-  paginationHTML += "<li><a href="#">2</a></li>";
-  paginationHTML += "<li><a href="#">3</a></li>";
-  paginationHTML += "<li><a href="#">4</a></li>";
-  paginationHTML += "</ul>";
+const pagination = studentList => {
+  const paginationDiv = document.createElement('div'); // create pagination div
+  paginationDiv.className = 'pagination'; // add pagination class to div
+  const ul = document.createElement('ul'); 
+  paginationDiv.appendChild(ul); // add ul to pagination div 
+  pageDiv.appendChild(paginationDiv); // add pagination div to end of page main div
+  
+  // Add paginated list item based off total users per page 
+  for(let i = 1; i < totalPages; i++) {
+    const li = document.createElement('li');
+    const link = document.createElement('a');
+    link.href = "#";
+    link.text = i;
+    li.appendChild(link);
+    ul.appendChild(li);
+
+    //If active page 1, add CSS class of Active
+    if (i === 1) {
+      link.classList.add('active');
+    }
+
+    // Add and remove Active class onClick of paginated items
+    ul.addEventListener('click', e => {
+      if (e.target === link) {
+        showPage(e.target.text, studentList);
+        document.querySelectorAll('.pagination a').forEach( el => el.classList.remove('active'));
+        e.target.classList.add('active');
+      }
+    })
+  }
 }
 
-pagination(totalPages);
-
-// <ul>
-//           <li>
-//             <a class="active" href="#">1</a>
-//           </li>
-//            <li>
-//             <a href="#">2</a>
-//           </li>
-//            <li>
-//             <a href="#">3</a>
-//           </li>
-//            <li>
-//             <a href="#">4</a>
-//           </li>
-//            <li>
-//             <a href="#">5</a>
-//           </li>
-//         </ul>
-//       </div>
+pagination(studentList);
