a5e26859d3a838915f2949f5af18fb254e423500
Tim Marks <timmarks90@gmail.com>
2018-04-29 20:25:46 -0700

    Added Search functionality
    Search functionality to the project

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@ const studentList = document.querySelectorAll('.student-item');
 const pageLimit = 10;
 const pageDiv = document.querySelector('.page');
 // Calculate number of pages to show based off the total number of students in list
-const totalPages = Math.floor(studentList.length / pageLimit);
+const totalPages = Math.ceil(studentList.length / pageLimit);
+const pageHeader = document.querySelector('.page-header');
 
 // Hide all but 10 students in list on page load. Each page will only show 10 students.
 const showPage = (number, list) => {
@@ -34,7 +35,7 @@ const pagination = studentList => {
     li.appendChild(link);
     ul.appendChild(li);
 
-    //If active page 1, add CSS class of Active
+    // If active page 1, add CSS class of Active
     if (i === 1) {
       link.classList.add('active');
     }
@@ -51,3 +52,41 @@ const pagination = studentList => {
 }
 
 pagination(studentList);
+
+// Create search field elements in header to search through list of students
+const search = list => {
+  const searchDiv = document.createElement('div');
+  searchDiv.className = 'student-search';
+  const searchDivInput = document.createElement('input');
+  searchDivInput.placeholder = "Search for students...";
+  const searchButton = document.createElement('button');
+  searchButton.innerHTML = "Search";
+  searchDiv.appendChild(searchDivInput);
+  searchDiv.appendChild(searchButton);
+  pageHeader.appendChild(searchDiv);
+
+  // Grab search input contents on click of submit and return list items that match
+  searchButton.addEventListener('click', list => {
+    let matchedList = [];
+    let searchValue = searchDivInput.value.toLocaleLowerCase();
+    for (let i = 0; i < studentList.length; i++) {
+      studentList[i].style.display = 'none';
+      let studentNames = studentList[i].querySelector('.student-details h3').innerHTML.toLowerCase();
+      let studentEmail = studentList[i].querySelector('.student-details .email').innerHTML.toLowerCase();
+      let doesContainName = studentNames.search(searchValue);
+      let doesContainEmail = studentEmail.search(searchValue);
+      if (doesContainName != -1 || doesContainEmail != -1) {
+        matchedList.push(studentList[i]);
+        studentList[i].removeAttribute('style');
+      }
+    }
+    // Alert popup if no search results returned
+    if (matchedList <= 0) {
+      alert("no students found");
+    }
+    // Add pagination and limit to max per page
+    showPage(1, matchedList); 
+  })
+}
+
+search(studentList);
